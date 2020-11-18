6dbf247613457485fadb56554d25f22484d1ff5d
Dutovkb <dutovapple@yandex.ru>
2020-11-18 09:07:08 +0300

    Initial Commit

---
+++ Study Autolayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Study Autolayout
+//
+//  Created by Кирилл Дутов on 18.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
